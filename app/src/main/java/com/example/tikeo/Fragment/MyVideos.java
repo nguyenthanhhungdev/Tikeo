9a01a139f5b4b2fb138dac62193c61d3375954ee
Nguyễn Thành Hưng <nguyenthanhhung.dev@gmail.com>
2023-10-31 15:52:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tikeo/Fragment/MyVideos.java
@@ -0,0 +1,7 @@
+package com.example.tikeo.Fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class MyVideos extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/tikeo/Models/Video.java
@@ -0,0 +1,49 @@
+package com.example.tikeo.Models;
+
+import android.net.Uri;
+
+public class Video {
+    private Uri uri;
+    private String name;
+    private int duration;
+    private int size;
+
+    public Video(Uri uri, String name, int duration, int size) {
+        this.uri = uri;
+        this.name = name;
+        this.duration = duration;
+        this.size = size;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setUri(Uri uri) {
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
